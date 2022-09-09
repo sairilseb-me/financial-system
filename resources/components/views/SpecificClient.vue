26748bb50fbbffc763b0730233e313ae21500bac
SairilSeb <cyrilseberiaga@gmail.com>
2022-09-09 19:15:05 +0800

    Create Specific Client page

---
+++ resources/components/views/SpecificClient.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container flex">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <h5 class="card-header">Featured</h5>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <h1>Assistance Data</h1>
+                <table class="table table-striped">
+                   <thead>
+                        <tr>
+                            <th>
+                                Assistance Type
+                            </th>
+                            <th>
+                                Category
+                            </th>
+                            <th>Patient Name</th>
+                            <th>Date Requested</th>
+                        </tr>
+                   </thead>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
