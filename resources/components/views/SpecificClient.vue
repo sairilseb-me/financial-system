651cf638c1ca2073b08b4c7760b6590d0f56384d
SairilSeb <cyrilseberiaga@gmail.com>
2022-09-09 19:38:01 +0800

    Create function for Specific Client

--- resources/components/views/SpecificClient.vue
+++ resources/components/views/SpecificClient.vue
@@ -1,33 +1,57 @@
 <template>
-    <div class="container flex">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card">
-                    <h5 class="card-header">Featured</h5>
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
+    <div class="container flex mt-3">
+        <div>
+            <div v-if="!userStore.user">
+                <div class="flex justify-content-center">
+                    <p>No data for now.</p>
                 </div>
             </div>
-            <div class="col-md-8">
-                <h1>Assistance Data</h1>
-                <table class="table table-striped">
-                   <thead>
-                        <tr>
-                            <th>
-                                Assistance Type
-                            </th>
-                            <th>
-                                Category
-                            </th>
-                            <th>Patient Name</th>
-                            <th>Date Requested</th>
-                        </tr>
-                   </thead>
-                </table>
+            <div v-else class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Special title treatment</h4>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <h1>Assistance Data</h1>
+                    <table class="table table-striped">
+                    <thead>
+                            <tr>
+                                <th>
+                                    Assistance Type
+                                </th>
+                                <th>Patient Name</th>
+                                <th>Date Requested</th>
+                                <th>Assistance Details</th>
+                            </tr>
+                    </thead>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+import useUserStore from '../../js/Store/UsersStore';
+import router from '../../js/router.js';
+
+import { onMounted } from 'vue';
+export default {
+    setup() {
+        let userStore = useUserStore();
+
+        let loadUserData = () =>{
+            axios.get(`http://127.0.0.1:8000/api/clients/view/${router.params.id}`)
+        }
+
+        return {
+            userStore,
+        }
+    },
+}
+</script>
