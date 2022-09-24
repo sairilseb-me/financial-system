bcc11ea1c83f81f4ff014c109262406fcf067917
SairilSeb <cyrilseberiaga@gmail.com>
2022-09-24 21:03:08 +0800

    Configured Dashboard Page

---
+++ resources/components/UI/Charts.vue
@@ -0,0 +1,48 @@
+<template>
+    <bar-chart></bar-chart>
+</template>
+
+<script>
+
+import {Bar} from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+export default {
+    props: {
+        chartId: {
+            type: String,
+            default: 'bar-chart'
+        },
+        datasetIdKey: {
+            type: String,
+            default: 'label'
+        },
+        width: {
+            type: Number,
+            default: 200
+        },
+        height: {
+            type: Number,
+            default: 200
+        },
+        cssClasses: {
+            default: '',
+            type: String
+        },
+        styles: {
+            type: Object,
+            default: () => {}
+        },
+        plugins: {
+            type: Object,
+            default: () => {}
+        }
+    },
+    components: {
+        'bar-chart': Bar,
+    },
+    setup(props) {
+        
+    },
+}
+</script>
